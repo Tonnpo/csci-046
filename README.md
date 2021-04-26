984ad05c28d4e6983147ed8f4166831bdaf41e87
Tonnpo <Tonn_po@hotmail.com>
2021-04-26 03:09:54 -0700

    heap hw

--- README.md
+++ README.md
@@ -2,5 +2,4 @@
 
 Build status:
 
-1. [![](https://github.com/Tonnpo/csci-046/workflows/tests-AVLTree/badge.svg)](https://github.com/Tonnpo/containers/actions?query=workflow%3Atests-AVLtree)
-1. [![](https://github.com/Tonnpo/csci-046/workflows/tests-BST/badge.svg)](https://github.com/Tonnpo/containers/actions?query=workflow%3Atests-BST)
+1. [![](https://github.com/Tonnpo/csci-046/workflows/tests-Heap/badge.svg)](https://github.com/Tonnpo/containers/actions?query=workflow%3Atests-Heap)
--- containers/Heap.py
+++ containers/Heap.py
@@ -1,21 +1,9 @@
-'''
-This file implements the Heap data structure as a subclass of the BinaryTree.
-The book implements Heaps using an *implicit* tree with an *explicit* vector implementation,
-so the code in the book is likely to be less helpful than the code for the other data structures.
-The book's implementation is the traditional implementation because it has a faster constant factor
-(but the same asymptotics).
-This homework is using an explicit tree implementation to help you get more practice with OOP-style programming and classes.
-'''
-
 from containers.BinaryTree import BinaryTree, Node
 
 
-class Heap():
+class Heap(BinaryTree):
     '''
-    FIXME:
     Heap is currently not a subclass of BinaryTree.
-    You should make the necessary changes in the class declaration line above
-    and in the constructor below.
     '''
 
     def __init__(self, xs=None):
@@ -24,94 +12,143 @@ class Heap():
         If xs is a list (i.e. xs is not None),
         then each element of xs needs to be inserted into the Heap.
         '''
+        super().__init__()
+        self.size = 0
+        if xs is not None:
+            self.insert_list(xs)
 
     def __repr__(self):
-        '''
-        Notice that in the BinaryTree class,
-        we defined a __str__ function,
-        but not a __repr__ function.
-        Recall that the __repr__ function should return a string that can be used to recreate a valid instance of the class.
-        Thus, if you create a variable using the command Heap([1,2,3])
-        it's __repr__ will return "Heap([1,2,3])"
-
-        For the Heap, type(self).__name__ will be the string "Heap",
-        but for the AVLTree, this expression will be "AVLTree".
-        Using this expression ensures that all subclasses of Heap will have a correct implementation of __repr__,
-        and that they won't have to reimplement it.
-        '''
         return type(self).__name__ + '(' + str(self.to_list('inorder')) + ')'
 
     def is_heap_satisfied(self):
-        '''
-        Whenever you implement a data structure,
-        the first thing to do is to implement a function that checks whether
-        the structure obeys all of its laws.
-        This makes it possible to automatically test whether insert/delete functions
-        are actually working.
-        '''
         if self.root:
             return Heap._is_heap_satisfied(self.root)
         return True
 
     @staticmethod
     def _is_heap_satisfied(node):
-        '''
-        FIXME:
-        Implement this method.
-        '''
+        ret = True
+        if node.left:
+            if node.value > node.left.value:
+                ret = False
+            else:
+                ret &= Heap._is_heap_satisfied(node.left)
+        if node.right:
+            if node.value > node.right.value:
+                ret = False
+            else:
+                ret &= Heap._is_heap_satisfied(node.right)
+        return ret
 
     def insert(self, value):
         '''
         Inserts value into the heap.
-
-        FIXME:
-        Implement this function.
-
-        HINT:
-        The pseudo code is
-        1. Find the next position in the tree using the binary representation of the total number of nodes
-            1. You will have to explicitly store the size of your heap in a variable (rather than compute it) to maintain the O(log n) runtime
-            1. See https://stackoverflow.com/questions/18241192/implement-heap-using-a-binary-tree for hints
-        1. Add `value` into the next position
-        1. Recursively swap value with its parent until the heap property is satisfied
-
-        HINT:
-        Create a @staticmethod helper function,
-        following the same pattern used in the BST and AVLTree insert functions.
         '''
+        self.size += 1
+        if self.root:
+            pos_bin = '{0:b}'.format(self.size)[1:]
+            Heap._insert(value, pos_bin, self.root)
+            if not self.is_heap_satisfied():
+                while not self.is_heap_satisfied() and pos_bin:
+                    Heap._fix_heap(pos_bin, pos_bin[-1], self.root)
+                    pos_bin = pos_bin[:-1]
+        else:
+            self.root = Node(value)
+
+    @staticmethod
+    def _insert(value, pos_bin, node):
+        if pos_bin:
+            if pos_bin[0] == '0':
+                if node.left:
+                    Heap._insert(value, pos_bin[1:], node.left)
+                else:
+                    node.left = Node(value)
+            else:
+                if node.right:
+                    Heap._insert(value, pos_bin[1:], node.right)
+                else:
+                    node.right = Node(value)
+
+    @staticmethod
+    def _fix_heap(pos_bin, child_pos, node):
+        parent_bin = pos_bin[:-1]
+        while parent_bin:
+            if parent_bin[0] == '0':
+                node = node.left
+                parent_bin = parent_bin[1:]
+            else:
+                node = node.right
+                parent_bin = parent_bin[1:]
+        if child_pos == '0':
+            if node.value > node.left.value:
+                temp = node.value
+                node.value = node.left.value
+                node.left.value = temp
+        elif child_pos == '1':
+            if node.value > node.right.value:
+                temp = node.value
+                node.value = node.right.value
+                node.right.value = temp
 
     def insert_list(self, xs):
-        '''
-        Given a list xs, insert each element of xs into self.
-
-        FIXME:
-        Implement this function.
-        '''
+        for x in xs:
+            self.insert(x)
 
     def find_smallest(self):
-        '''
-        Returns the smallest value in the tree.
-
-        FIXME:
-        Implement this function.
-        '''
+        if self.is_heap_satisfied():
+            return self.root.value
 
     def remove_min(self):
-        '''
-        Removes the minimum value from the Heap.
-        If the heap is empty, it does nothing.
+        if self.root:
+            if self.size == 1:
+                self.root = None
+                self.size -= 1
+            else:
+                Heap._remove_min(self.size, self.root)
+                self.size -= 1
 
-        FIXME:
-        Implement this function.
-
-        HINT:
-        The pseudocode is
-        1. remove the bottom right node from the tree
-        2. replace the root node with what was formerly the bottom right
-        3. "trickle down" the root node: recursively swap it with its largest child until the heap property is satisfied
-
-        HINT:
-        I created two @staticmethod helper functions: _remove_bottom_right and _trickle.
-        It's possible to do it with only a single helper (or no helper at all),
-        but I personally found dividing up the code into two made the most sense.
-        '''
+    @staticmethod
+    def _remove_min(size, node):
+        pos_bin = '{0:b}'.format(size)[1:]
+        last_added = None
+        curr_node = node
+        while pos_bin:
+            if pos_bin[0] == '0':
+                if len(pos_bin) == 1:
+                    last_added = curr_node.left.value
+                    curr_node.left = None
+                    pos_bin = pos_bin[1:]
+                else:
+                    curr_node = curr_node.left
+                    pos_bin = pos_bin[1:]
+            else:
+                if len(pos_bin) == 1:
+                    last_added = curr_node.right.value
+                    curr_node.right = None
+                    pos_bin = pos_bin[1:]
+                else:
+                    curr_node = curr_node.right
+                    pos_bin = pos_bin[1:]
+        node.value = last_added
+        while not Heap._is_heap_satisfied(node):
+            if node.right and node.left:
+                if node.right.value < node.left.value:
+                    temp = node.value
+                    node.value = node.right.value
+                    node.right.value = temp
+                    node = node.right
+                else:
+                    temp = node.value
+                    node.value = node.left.value
+                    node.left.value = temp
+                    node = node.left
+            elif node.right and node.left is None:
+                temp = node.value
+                node.value = node.right.value
+                node.right.value = temp
+                node = node.right
+            elif node.left and node.right is None:
+                temp = node.value
+                node.value = node.left.value
+                node.left.value = temp
+                node = node.left
