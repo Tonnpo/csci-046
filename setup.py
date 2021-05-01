9de7968a0d2b2e3e987327f14cd3a8e480920052
Tonnpo <Tonn_po@hotmail.com>
2021-05-01 03:50:44 -0700

    change setup

--- setup.py
+++ setup.py
@@ -1,18 +1,25 @@
 import pathlib
-from setuptools import setup
+from setuptools import find_packages, setup
 
 HERE = pathlib.Path(__file__).parent
 
 README = (HERE / "README.md").read_text()
 
 setup(
-        name="cmc_csci046_data_structure_Pothis",
+        name="cmc_csci046_Pothis",
         version="1.0.0",
-        description="Data Structure and unicode",
+        description="Implementation of fundamental data structures and unicode normalization",
         long_description=README,
+        long_description_content_type="text/markdown",
         url="https://github.com/Tonnpo/csci-046",
         author="Ton Pothisawang",
-        author_email="Tonn_po@hotmail.com",
-        license="BSD3",
-        packages=[ "containers" ]
-    )
+        author_email="Tpothis@students.pitzer.edu",
+        license="BSD-3-Clause",
+        classifiers=[
+            "License :: OSI Approved :: BSD-3-Clause License",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.7",
+        ],
+        packages=find_packages(exclude=("tests")),
+        include_package_data=True,
+)
